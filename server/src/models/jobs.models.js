83fac20686f5cb45b2e5e8f1f3bfc1bcc28cec93
nishant25kr <nishant927472@gmail.com.com>
2025-07-26 18:10:21 +0530

    add users and jobs schema

---
+++ server/src/models/jobs.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"JobName is required"]
+    },
+    title:{
+        type:String,
+        required:[true,"JobTitle is required"]
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    Type:{
+        type:String,
+        required:true,
+        enum:["Remote","Onsite"]
+    },
+    place:{
+        type:String,
+        required:true,
+    },
+    salary:{
+        type:Number,
+        required:true
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+
+
+},{timestamps:true})
+
+export const Job = mongoose.model("Job",jobSchema);
