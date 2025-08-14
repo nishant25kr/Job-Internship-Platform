9d172aaa330eb69c1e6e0f04b65eefde04137a64
nishant25kr <nishant927472@gmail.com>
2025-08-14 12:48:12 +0530

    up

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -1,4 +1,4 @@
-import User from "../models/User.models.js";
+import User from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
